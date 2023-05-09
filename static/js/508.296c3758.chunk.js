df0c4a824096d6523478f57c97f3fd3581fc1ff8
Tritolium <paracrafta@gmail.com>
2023-05-09 14:27:28 +0200

    build

---
+++ static/js/508.296c3758.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkspz_planner=self.webpackChunkspz_planner||[]).push([[508],{3508:function(e,n,p){p.r(n),n.default={}}}]);
+//# sourceMappingURL=508.296c3758.chunk.js.map
