c1061c16a985e8aa1e85475cedcd2215001a9bf4
Dominik Poggel <pog@iesy.com>
2024-03-05 11:28:24 +0100

    build v0.13

--- static/js/508.296c3758.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkspz_planner=self.webpackChunkspz_planner||[]).push([[508],{3508:function(e,n,p){p.r(n),n.default={}}}]);
-//# sourceMappingURL=508.296c3758.chunk.js.map
