c73111189e7b3ca2af726a3f29c6384f02e4ca4a
Dominik Poggel <pog@iesy.com>
2025-01-08 12:28:28 +0100

    deploy

---
+++ static/js/586.59359e45.chunk.js
@@ -0,0 +1,22 @@
+"use strict";(globalThis.webpackChunkspz_planner=globalThis.webpackChunkspz_planner||[]).push([[586],{1586:(e,n,t)=>{t.r(n),t.d(n,{default:()=>r});var s=t(5043);const a=t(5464).Ay.form`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    max-width: 6cm;
+    max-height: 4cm;
+    width: 90vw;
+
+    input {
+        margin: 1px;
+        width: 100%;
+        height: 33%;
+        min-height: 0.5cm;
+    }
+
+    button {
+        width: max-content;
+        margin: 1px;
+    }
+`;var i=t(5813),l=t(579);const r=e=>{const n=(0,s.useCallback)((async n=>{n.preventDefault();let t=document.getElementById("loginname").value,s=document.getElementById("passwd").value;e.sendLogin(t,(0,i.sha256)(s))}),[e]);return(0,l.jsxs)(a,{onSubmit:n,children:[(0,l.jsx)("input",{id:"loginname",type:"text",placeholder:"Namen eingeben"}),(0,l.jsx)("input",{id:"passwd",type:"password",placeholder:"Passwort","aria-label":"Passwort"}),(0,l.jsx)("i",{children:"Passwort nur notwendig, wenn schon eins vergeben wurde, standardm\xe4\xdfig nicht der Fall"}),(0,l.jsx)("button",{type:"submit",children:"Login"}),(0,l.jsx)("button",{onClick:e=>{e.preventDefault(),window.location.href="http://wa.me/4915157616812?text=Hallo, ich habe mein Passwort vergessen. Kannst du es mir bitte zur\xfccksetzen?"},children:"Passwort vergessen?"})]})}}}]);
+//# sourceMappingURL=586.59359e45.chunk.js.map
